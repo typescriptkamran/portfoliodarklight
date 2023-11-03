e03515d912f33f8780382047e31e64c7a4804b68
typescriptkamran <typescriptwithkamran@gmail.com>
2023-11-03 20:48:57 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+      
+      <body className={inter.className}>
+      <header>
+      <NavBar />
+      </header>
+        {children}
+      <footer></footer>
+      </body>
     </html>
   )
 }
